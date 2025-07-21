2e533c05c39cc8387e08db2f41f2b78a05e0fb25
GitaVera <131251309+GitaVera@users.noreply.github.com>
2025-07-21 14:56:30 +0300

    задача 1.4

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,10 @@
 function checkSpam(str) {
-  // ваш код...
+  let uppStr = str.toUpperCase()
+  let Bet = '1xBet'
+  let XXX = 'XXX'
+  let upp1xBet = Bet.toUpperCase()
+  let uppXXX = XXX.toUpperCase()
+
+  return (uppStr.indexOf(uppXXX) !== -1 || uppStr.indexOf(upp1xBet) !== -1)
+
 }
